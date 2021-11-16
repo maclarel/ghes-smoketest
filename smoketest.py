6c7291c5ee8c45e371abbfa0d5fe0db4470d8227
Logan MacLaren <maclarel@github.com>
2021-11-16 09:09:20 -0500

    starting refactor

--- smoketest.py
+++ smoketest.py
@@ -2,6 +2,7 @@ import argparse
 import requests
 import logging
 import json
+import pytest
 
 '''
 Usage: smoketest.py [-p] -pat PERSONAL_ACCESS_TOKEN [-t] -target GHES_URL [-n] -num NUM_REPOS [-debug]
@@ -19,7 +20,8 @@ Optional arguments:
 
 pat = ""
 target = ""
-num_repos = 10
+num_repos = 1 
+headers = {'Accept': 'application/vnd.github.v3+json', 'Authorization': 'token ' + pat}
 
 
 def validate_args():
@@ -53,83 +55,17 @@ def repo_list():
     # Create names of repositories to use
     return [f"smoketest_repo{num}" for num in range(1, num_repos+1)]
 
-
-def test_repo_creation():
-    # Test creation of repositories
-    logging.info("Testing creation of smoketest repositories")
-    logging.debug(f"Creating the following repositories: {repo_list()}")
-    url = construct_api_url('user/repos')
-    error_count = 0
-    for r in repo_list():
-        payload = {
-            'name': r
-        }
-        response = requests.post(url, headers=headers,
-                                 data=json.dumps(payload), verify=False)
-        # We expect a 201 response for confirmed repo creation
-        if response.status_code != 201:
-            error_count += 1
-    logging.info(
-        f"Repostiory creation returned {error_count} errors out of {num_repos} attempts. {float(error_count/num_repos)*100}% failure rate.")
-
-
-def test_repo_deletion():
-    # Delete repositories created in test_repo_creation
-    logging.info("Testing deletion of smoketest repositories")
-    logging.debug("Deleting the following repsitories: {repo_list()}")
-    username = get_pat_user(pat)
-    error_count = 0
-    for r in repo_list():
-        url = construct_api_url(f'repos/{username}/{r}')
-        response = requests.delete(url, headers=headers, verify=False)
-        # We expect a 204 response on positive repository deletion
-        if response.status_code != 204:
-            error_count += 1
-    logging.info(
-        f"Repository deletion returned {error_count} errors out of {num_repos} attempts. {float(error_count/num_repos)*100}% failure rate.")
-
-
-def test_issues():
-    # Creates an issue in each smoketest repository, then deletes it
-    logging.info("Testing creation of Issues in smoketest repositories")
-    logging.debug(
-        f"Creating an Issue in each of the following repostiories: {repo_list()}")
-    username = get_pat_user(pat)
-    error_count = 0
-    payload = {
-        'title': 'This is a test issue'
-    }
-    for r in repo_list():
-        url = construct_api_url(f'repos/{username}/{r}/issues')
-        response = requests.post(url, headers=headers,
-                                 data=json.dumps(payload), verify=False)
-        # We expect a 201 response on positive issue creation
-        if response.status_code != 201:
-            error_count += 1
-    logging.info(
-        f"Issue creation returned {error_count} errors out of {num_repos} attempts. {float(error_count/num_repos)*100}% failure rate.")
-
-
-def test_file_creation():
-    # Creates a file in each smoketest repository
-    logging.info("Testing file creation in each smoketest repository")
-    logging.debug(
-        f"Creating a single file in each of the following repositories: {repo_list()}")
-    username = get_pat_user(pat)
-    error_count = 0
-    payload = {
-        "message": "message",
-        "content": "Zm9vCg=="  # 'foo'
-    }
-    for r in repo_list():
-        url = construct_api_url(f'repos/{username}/{r}/contents/testfile')
-        response = requests.put(url, headers=headers,
-                                data=json.dumps(payload), verify=False)
-        # We expect a 201 response on positive file creation
-        if response.status_code != 201:
-            error_count += 1
-    logging.info(
-        f"File creation returned {error_count} errors out of {num_repos} attempts. {float(error_count/num_repos)*100}% failure rate.")
+def api_call(endpoint, method, expected_status, payload=None):
+    # Test endpoint passed in to function
+    logging.info(f"Testing {endpoint} endpoint")
+    url = construct_api_url(endpoint)
+    if payload:
+        response = requests.post(url, headers = headers, data=json.dumps(payload), verify=False)
+    else:
+        response = requests.post(url, headers = headers, verify=False)
+    if response.status_code != expected_status:
+        return False
+    return True
 
 
 if __name__ == '__main__':
@@ -162,10 +98,17 @@ if __name__ == '__main__':
     if server_up():
         logging.info(
             f"Running as {get_pat_user(pat)} - PAT auth confirmed working")
-        test_repo_creation()
-        test_issues()
-        test_file_creation()
-        test_repo_deletion()
+        for r in repo_list():
+            api_call('user/repos', 'get', 201, {'name': r})
+            # create issue
+            # create file
+            # delete repo
     else:
         logging.info(
             "Server appears to be down or is not a GitHub Enterprise Server system. Please double check the URL.")
+
+
+# Tests
+
+def test_repo():
+    assert api_call('user/repos', 'get', 201, {'name':'foo'}) is True
